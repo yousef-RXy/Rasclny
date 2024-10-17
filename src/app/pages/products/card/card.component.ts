e78c6c19e173a9cf79f7deb1b9f4b5f1603daa92
mahmoud-Riad00 <faydmhmwd1@gmail.com>
2024-10-17 18:54:33 +0300

    Bayyaaaa3-5odaaar

--- src/app/pages/products/card/card.component.ts
+++ src/app/pages/products/card/card.component.ts
@@ -1,23 +1,3 @@
-<<<<<<< HEAD
-import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
-
-@Component({
-  selector: 'app-card',
-  templateUrl: './card.component.html',
-  styleUrls: ['./card.component.css']
-})
-export class CardComponent implements OnInit {
-  cartProducts: any[] = [];
-  @Input() singleCategory?: any;
-  @Output() cartProductsChange = new EventEmitter<any[]>();
-
-  ngOnInit(): void {}
-
-  cartProduct(ProductId: number) {
-    this.cartProducts.push(ProductId);
-    this.cartProductsChange.emit(this.cartProducts);  // Notify parent component
-    console.log(this.cartProducts);
-=======
 import { Component, Input, OnInit } from '@angular/core';
 import { CartService } from '../../../services/cart.service';
 import { products } from 'src/app/types/dataType';
@@ -34,6 +14,5 @@ export class CardComponent implements OnInit {
     event.stopPropagation();
     this.cart.addToCart(product);
     console.log(this.cart.getState());
->>>>>>> main
   }
 }
